e344227e63744bec8b6795275ff214ab709c101a
black <shutcing@gmail.com>
2023-11-14 23:47:54 +0500

    Cool fixes

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -7,6 +7,8 @@ namespace HelloApp
         static void Main (string[] args)
         {
             Console.WriteLine("hello everyone and Merry X-mas");
+            GreetBlack();
+            GreetWhite();
         }
 
         static void GreetWhite ()
